ed467ecdef2a92d837253e5da267b0dbcd97e24d
WingYee Mak <wwymak@gmail.com>
2016-09-17 18:48:43 +0100

    some tweaks to the various bits

--- parser.js
+++ parser.js
@@ -1,7 +1,9 @@
 "use strict";
 const fs = require('fs');
 const async = require('async');
+const iconv = require('iconv-lite');
 const dataFilePath = './dataFolderS3';
+// const dataFilePath = './dataFolder';
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
 const mongoURI = 'mongodb://localhost:27017/instagramLondon';
@@ -16,6 +18,7 @@ mongoClient.connect(mongoURI, function(err, db) {
                     console.log('err in readdir');
                     next(err);
                 } else {
+                    files.sort((a, b) =>  {if(a<b){return 1} if(a >b) {return -1} return 0});
                     console.log('file names:', files.length, files[0]);
                     next(null, files);
                 }
@@ -58,9 +61,9 @@ function parseOneFile(fileContentArr) {
 function parseAndWriteToDB(fileName, collection, callback) {
     console.log('fileName: ', fileName);
     async.waterfall([
-        function readFile(next){
+        function readFile(next) {
             fs.readFile(`${dataFilePath}/${fileName}`, 'utf8', (err, data) => {
-                if(err) {
+                if (err) {
                     console.log('read file err');
                     next(err);
                 } else {
@@ -70,20 +73,22 @@ function parseAndWriteToDB(fileName, collection, callback) {
         },
         function parseFileToArr(data, next) {
             var parsed = parseOneFile(data);
-            parsed.forEach(d =>{ d.timestamp = new Date(parseInt(d.created_time) * 1000)})
+            parsed.forEach(d => {
+                d.timestamp = new Date(parseInt(d.created_time) * 1000)
+            });
             next(null, parsed);
         },
         function writeArrToDB(arr, next) {
             console.log('writeArrToDB');
-            async.mapLimit(arr,3, function (data, cb) {
+            async.mapLimit(arr, 3, function (data, cb) {
                 collection.insertOne(data, (err, result) => {
-                    if(err) {
+                    if (err) {
                         // console.log(err)
                     }
                     cb(null, result);
                 });
             }, function (err, res) {
-                if(err) {
+                if (err) {
                     next(err);
                 } else {
                     next(null)
@@ -91,13 +96,10 @@ function parseAndWriteToDB(fileName, collection, callback) {
             });
         }
     ], function (err, result) {
-        if(err) {
+        if (err) {
             callback(err);
         } else {
             callback(null);
         }
     })
 }
-
-
-
